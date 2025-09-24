37bd7b4d70c8c5844f25b62edce7596cef869213
Monika Magar <magarmonika2002@gmail.com>
2025-09-24 20:57:40 +0530

    Add Render deployment configuration

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -17,6 +17,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Loads environment variables from a .env file
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
