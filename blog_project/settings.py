0959abc25da1145528a900aa90e752e9228b2d77
Monika Magar <magarmonika2002@gmail.com>
2025-09-24 20:09:00 +0530

    blog project

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -77,29 +77,19 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blogdb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Monika@2002',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
+        'NAME': 'blogdb',
         'USER': 'postgres',
-        'PASSWORD': 'wcGzLJpCFIBjLnPfgMWezGAniQEePRmz',
-        'HOST': 'switchyard.proxy.rlwy.net',
-        'PORT': '29245',
+        'PASSWORD': 'Monika@2002',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
 
 
 # Password validation
